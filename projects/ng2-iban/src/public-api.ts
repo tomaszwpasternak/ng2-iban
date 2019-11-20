99e1988a8fdafa139f1302efd981dc732c3b4734
Tomasz Pasternak <tomasz.pasternak@nextep-technologies.com>
2019-11-20 09:34:43 +0100

    fix public api

--- projects/ng2-iban/src/public-api.ts
+++ projects/ng2-iban/src/public-api.ts
@@ -2,6 +2,11 @@
  * Public API Surface of ng2-iban
  */
 
-export * from './lib/ng2-iban.service';
-export * from './lib/ng2-iban.component';
 export * from './lib/ng2-iban.module';
+
+export * from './lib/pipe/ng2-iban.pipe';
+export * from './lib/pipe/ng2-iban-option.interface';
+
+export * from './lib/service/ng2-iban.service';
+
+export * from './lib/validator/ng2-iban.validator';
